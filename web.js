2a9d99d95830b567d946b796e6a22d1dff35a8aa
GarryLaly <garrylaly@gmail.com>
2014-02-12 05:34:48 +0700

    First test :)

---
+++ web.js
@@ -0,0 +1,14 @@
+// web.js
+var express = require("express");
+var app = express();
+
+app.use(express.logger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
